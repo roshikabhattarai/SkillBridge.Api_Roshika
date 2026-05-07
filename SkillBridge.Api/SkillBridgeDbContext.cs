1f6a408dd03e4c61be15a36d9ced7e70748e09df
Roshika-Bhattarai <roshikabhattarai74@gmail.com>
2026-05-07 08:30:32 +0545

    Db Create

---
+++ SkillBridge.Api/SkillBridgeDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SkillBridge.Api.Entities;
+
+public class SkillBridgeDbContext : DbContext
+{
+    public SkillBridgeDbContext(DbContextOptions<SkillBridgeDbContext>options) : base(options)
+    {
+        
+    }
+    public DbSet<Users> Users {get; set; } 
+}
